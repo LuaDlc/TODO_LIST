2d4b7eafaf63d1ce6675521e564339701a281513
LuaDlc <luadiaslc@gmail.com>
2022-07-14 13:26:57 -0300

    criei o todo list e iniciei a implementação do mobx

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -1,22 +1,23 @@
-import 'package:exemplo/todo_list_page.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_list/todo_list_page.dart';
+
 //estrutura básica
 main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(Key: key);
-  
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-    primaryColor: Colors.purple,
-    ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.lightGreen,
+      ),
       home: TodoListPage(),
-    ),
+    );
   }
 }
 //estrutura básica-fim
--- todo_list/lib/todo_list_page.dart
+++ todo_list/lib/todo_list_page.dart
@@ -1,84 +1,81 @@
 import 'package:flutter/material.dart';
 
-class TodoListPage extends StateFulWidget {
+class TodoListPage extends StatefulWidget {
   const TodoListPage({Key? key}) : super(key: key);
-  
+
   @override
   _TodoListPageState createState() => _TodoListPageState();
 }
 
-class _TodoListPageState extends State<TodoListpage> {
- 
-  TextEditingController _texteEditingController = textEditingController();
-  
+class _TodoListPageState extends State<TodoListPage> {
+  TextEditingController _textEditingController = TextEditingController();
+
   List<String> tarefas = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold{
+    return Scaffold(
       appBar: AppBar(
         title: Text('Lista de tarefas'),
       ),
       //corpo do aplicativo:
-      body: ,Container(
-      padding: EdgeInsets.all(24),
+      body: Container(
+        padding: EdgeInsets.all(24),
         child: Column(
           children: [
             TextField(
-            controller: _texteEditingController,
-            ), //textField
-          //adicionando as tarefas na tela
+              controller: _textEditingController,
+            ),
+            //adicionando as tarefas na tela
             Container(
-            height: 400, //adiciona uma restrição de tamanho
-              child:ListView.separated(
-                separetedBuilder: separatorBuilder(context, index) => Divider(),//cria uma linha
-              itemCount:tarefas.length, //quantidade de itens dentro da lista
-              itemBUilder: (context, index) { //builder tem q retornar um widget
-                return LisTile(
-                  title: Text(tarefas[index]),
-                  onLongPress: () {
-                    setState(() {
-                      
-                      tarefas.removeAt(index);
-                      
-                    });
-                  }
-                ), //listTile
-              },
-              ), //listView.builder
-            ), //container
+              height: 400, //adiciona uma restrição de tamanho ao container
+              child: ListView.separated(
+                separatorBuilder: (context, index) =>
+                    Divider(), //cria uma linha
+                itemCount: tarefas.length, //pega tamanho da lista
+                itemBuilder: (context, index) {
+                  //builder tem q retornar um widge
+                  return ListTile(
+                      title: Text(tarefas[index]),
+                      onLongPress: () {
+                        setState(() {
+                          tarefas.removeAt(index);
+                        });
+                      }); //listTile
+                },
+              ),
+            ),
           ],
-      ), //column
-    ), //container
+        ),
+      ),
       floatingActionButton: Row(
         crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: mainAxisAlignment.end,
-        children[
-     floatingActionButton: FloatingActionButton(
-       backgroundCOlor: Colors.lime,
-       onPressed:(){
-         if (_texteEditingController.text.length> 0) {
-           SetState((){
-             tarefas.add(_texteEditingController.text);
-           });
-           _texteEditingController.clear();
-         }
-       },
-       child: Icon(Icons.add),
-     ), //floatingActionButton 
-          floatingActionButton: Row(
-        children[
-     floatingActionButton: FloatingActionButton(
-       backgroundCOlor: Colors.lime,
-       onPressed:(){
-           SetState((){
-             tarefas.add= [];
-           });
-           _texteEditingController.clear();
-       },
-       child: Icon(Icons.remove),
-     ),
-    ],
-   ), //row
-  }; //scaffold
- }
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.lime,
+            onPressed: () {
+              if (_textEditingController.text.length > 0) {
+                setState(() {
+                  tarefas.add(_textEditingController.text);
+                });
+                _textEditingController.clear();
+              }
+            },
+            child: Icon(Icons.add),
+          ), //floatingActionButton
+          FloatingActionButton(
+            backgroundColor: Colors.lime,
+            onPressed: () {
+              setState(() {
+                tarefas = [];
+              });
+              _textEditingController.clear();
+            },
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
 }
